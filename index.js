7a258ac891275eb2724f377e33c44d3eb26d23ef
DarlanDuarte <darlan.duarte.m@gmail.com>
2023-04-07 22:03:46 -0300

    Ajustando o EJS e implementando o BootStrap do Projeto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const sequelize = require('./database/database')
 
 const app = express()
 
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-
+sequelize.authenticate().then(() => console.log(`Conexão com o banco de dados feita com sucesso!`)).catch((error) => console.log(error))
 
 app.get('/', (req, res) =>{
     res.render('index')
