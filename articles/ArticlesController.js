a0b1a84619e2692b020595f08d40cd97337c7816
DarlanDuarte <darlan.duarte.m@gmail.com>
2023-04-09 15:39:05 -0300

    Começando a implementar os Artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/articles', (req, res) => {
-   res.send('Articles Criada Com Sucesso');
+  res.send('Articles Criada Com Sucesso');
+});
+
+router.get('/admin/articles/new', (req, res) => {
+  res.render('admin/articles/new');
 });
 
 module.exports = router;
