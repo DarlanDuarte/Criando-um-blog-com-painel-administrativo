cf0449314747b9e5554a023ef51faebe179d6315
DarlanDuarte <darlan.duarte.m@gmail.com>
2023-04-11 02:21:12 -0300

    Conclusão da HomePage

--- index.js
+++ index.js
@@ -25,9 +25,64 @@ app.use('/', categoriesController);
 app.use('/', artilesController);
 
 app.get('/', (req, res) => {
-  Article.findAll().then(articles => {
-    res.render('index', { articles: articles });
+  Article.findAll({
+    order: [['id', 'DESC']],
+  }).then(articles => {
+    Category.findAll().then(categories => {
+      res.render('index', { articles: articles, categories: categories });
+    });
   });
+});
+
+app.get('/:slug', (req, res) => {
+  const slug = req.params.slug;
+
+  Article.findOne({
+    where: {
+      slug: slug,
+    },
+  })
+    .then(articles => {
+      if (articles != undefined) {
+        Category.findAll().then(categories => {
+          res.render('articles', {
+            articles: articles,
+            categories: categories,
+          });
+        });
+      } else {
+        res.redirect('/');
+      }
+    })
+    .catch(error => {
+      res.redirect('/');
+    });
+});
+
+app.get('/category/:slug', (req, res) => {
+  const slug = req.params.slug;
+
+  Category.findOne({
+    where: {
+      slug: slug,
+    },
+    include: [{ model: Article }],
+  })
+    .then(category => {
+      if (category != undefined) {
+        Category.findAll().then(categories => {
+          res.render('index', {
+            articles: category.articles,
+            categories: categories,
+          });
+        });
+      } else {
+        res.redirect('/');
+      }
+    })
+    .catch(error => {
+      res.redirect('/');
+    });
 });
 
 app.listen(8080, () => {
